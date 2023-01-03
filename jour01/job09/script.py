0e09b9c0246e088742c35046661a1cd2d1e3d7a2
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-01-03 09:52:46 +0100

    modification pour aller plus loin

--- jour01/job09/script.py
+++ jour01/job09/script.py
@@ -3,14 +3,13 @@ i = 0
 bool = False
 string = input("Entrez la chaine de caractère dans laquelle vous voulez vérifier la présence d'un e: ")
 
-for i in range(len(string)):
+while i < len(string) and bool == False:
     if string[i] == target:
         print("Il y'a bien un e dans la chaine de caractère, il est en", (i+1),"ème position")
         bool = True
-        break
     else:
         i+=1
 
 if bool == False:
-    print("Il n'y a pas de e dans la chaine de caractère")
+    print("Il n'y à pas de e dans la chaine de caractère")
 
